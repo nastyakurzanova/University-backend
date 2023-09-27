24152c62fac44753ceeb1b89e4fb5187eade0d5d
nastyakurzanova <nastakurzanova26801@gmail.com>
2023-09-27 19:08:01 +0000

    Одна модель, исправлен css

--- rip_lab1/pythonproject/views.py
+++ rip_lab1/pythonproject/views.py
@@ -17,14 +17,14 @@ orders_arr = [
 
 
 info_arr =  [
-            {'title': '501ю', 'id': 501, 'src': 'image/185.jpg','definition': orders_arr[0]},
-            {'title': '306э', 'id': 306, 'src': 'image/1.jpg','definition': orders_arr[1]},
-            {'title': '222л', 'id': 222, 'src': 'image/222_1.jpg','definition': orders_arr[2]},
-            {'title': '362м', 'id': 362, 'src': 'image/362m.jpg','definition': orders_arr[3]},
-            {'title': '111', 'id': 111, 'src': 'image/home.jpg','definition': orders_arr[4]},
-            {'title': '903', 'id': 903, 'src': 'image/903.jpg','definition': orders_arr[5]},
-            {'title': '135л', 'id': 135, 'src': 'image/ulk.jpeg','definition': orders_arr[6]},
-            {'title': '323', 'id': 323, 'src': 'image/323.jpg','definition': orders_arr[7]},
+            {'title': '501ю', 'id': 501, 'src': 'image/185.jpg','corpus': 'Главное здание','price': 6000, 'info': 'Большая аудитория для интересных лекция'},
+            {'title': '306э', 'id': 306, 'src': 'image/1.jpg','corpus': 'Энерго', 'price': 7500,'info': 'Аудитория для лабораторных'},
+            {'title': '222л', 'id': 222, 'src': 'image/222_1.jpg','corpus': 'УЛК','price': 8000, 'info': 'Лекционная аудитория'},
+            {'title': '362м', 'id': 362, 'src': 'image/362m.jpg','corpus': 'СМ', 'price': 5000,'info': 'аудиториядля лабораторных'},
+            {'title': '111', 'id': 111, 'src': 'image/home.jpg','corpus': 'Главное здание', 'price': 12000,'info': 'Дом физики. Аудитория для лабораторных'},
+            {'title': '903', 'id': 903, 'src': 'image/903.jpg','corpus': 'Главное здание', 'price': 15000,'info': 'Аудитория кафедры ИУ5'},
+            {'title': '135л', 'id': 135, 'src': 'image/ulk.jpeg','corpus': 'УЛК', 'price': 4000,'info': 'Аудитория для семинаров'},
+            {'title': '323', 'id': 323, 'src': 'image/323.jpg','corpus': 'Главное здание','price': 3000, 'info': 'Аудитория для лекций по физике'},
         ]
 
 # def GetOrders(request):
